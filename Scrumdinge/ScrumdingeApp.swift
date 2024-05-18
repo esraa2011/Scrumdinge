3c59342286e0920aa6de8ef4449a17f96a26e6f3
Esraa Abdelfatah <esraa4255@gmail.com>
2024-05-18 14:29:05 +0300

    Initial Commit

---
+++ Scrumdinge/ScrumdingeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingeApp.swift
+//  Scrumdinge
+//
+//  Created by Esraa Abdelfatah on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
